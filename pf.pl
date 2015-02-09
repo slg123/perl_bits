5ffb4847fa057fb0ad498ce894bcac1a8f74a909
root <root@om.houqe.lab>
2015-02-09 11:30:04 -0600

    prime factorization cpu throttler

---
+++ pf.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my @factors = ();
+my @prime_factors = (); 
+
+my $bignum = 600851475143;
+
+my $i = 1;
+my $j;
+my $k;
+
+while ( $i <= $bignum ) {
+    $k = 0;
+    if ( $bignum % $i == 0 ) {
+        $j = 1;
+        while ( $j <= $i ) { 
+            if ( $i % $j == 0 ) {
+                $k++;
+            }
+            $j++;
+        }
+        if ( $k == 2 ) {
+            print "prime factor: $i\n"; 
+        }
+    }
+    $i++;
+}
+
+
